a0c3e38273bafba99b1bf3555b7db1c140628807
Simone Mauro <simon.mauro@virgilio.it>
2022-02-25 18:54:32 +0100

    Effetto hover informazioni film

--- src/components/myCard.vue
+++ src/components/myCard.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="card">
         <img :src="linkInizialeImmagini + film.poster_path" alt="thumbnail film">
-        <h1>{{film.title}}</h1>
-        <h2>{{film.original_title}}</h2>
-        <p>{{film.original_language}}</p>
-        <p>{{film.vote_average}}</p>
+
+        <div class="card-info">
+            <h1>{{film.title}}</h1>
+            <h2>{{film.original_title}}</h2>
+            <p class="language">{{film.original_language}}</p>
+            <p class="vote">{{film.vote_average}}</p>
+        </div>
+        
     </div>
 </template>
 
@@ -21,9 +25,41 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
     .card{
         margin: 15px;
         text-align: center;
+        position: relative;
+
+        .card-info{
+            display: none;
+        }
+
+        &:hover .card-info{
+            display: block;
+            cursor: pointer;
+        }
+
+        h1,h2,p{
+            color: white;
+            position: absolute;
+            left: 50%;
+            width: 100%;
+            transform: translate(-50%, 0);
+        }
+
+        h1{
+            top: 20px;
+        }
+
+        h2{
+            top: 200px;
+        }
+        p.language{
+            top: 300px;
+        }
+        p.vote{
+            top: 400px;
+        }
     }
 </style>
