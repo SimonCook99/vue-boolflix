380e8703d25af374b71670a4347def342daaf007
Simone Mauro <simon.mauro@virgilio.it>
2022-03-01 11:49:53 +0100

    milestone 4 completata

--- src/components/myCard.vue
+++ src/components/myCard.vue
@@ -2,28 +2,28 @@
     <div class="card">
         
         <img v-if="item.poster_path != null" :src="linkInizialeImmagini + item.poster_path" alt="thumbnail film/serie">
-        <div class="overlay"></div>
-
-        <div class="card-info">
+        <div class="overlay">
+            <div class="card-info">
             <h1>{{item.title== null ? item.name : item.title}}</h1>
             <h2>{{item.original_title == null ? item.original_name : item.original_title}}</h2>
             <lang-flag class="language" :iso="item.original_language"/>
-            <!-- <p class="language">{{item.original_language}}</p> -->
 
             <div v-if="item.vote_average != 0" class="vote">
 
                 <!--Soluzione con il componente font-awesome-icon-->
-                <font-awesome-icon v-for="(star, index) in Math.ceil(item.vote_average / 2)" :key="index" icon="fas fa-star" />
-                <font-awesome-icon v-for="(emptyStar, index2) in 5 - Math.ceil(item.vote_average / 2)" :key="index2" :icon="['far', 'fa-star']" />
+                <font-awesome-icon v-for="(star, index) in Math.ceil(item.vote_average / 2)" :key="'stella' + index" icon="fas fa-star" />
+                <font-awesome-icon v-for="(emptyStar, index) in 5 - Math.ceil(item.vote_average / 2)" :key="'stellaVuota' + index" :icon="['far', 'fa-star']" />
 
                 <!--soluzione con i normali tag i-->
                 <!-- <i v-for="(star, index) in Math.ceil(item.vote_average / 2)" :key="index" class="fas fa-star"></i> -->
                 <!-- <i v-for="(star, index) in 5 - Math.ceil(item.vote_average / 2)" :key="index" class="fa-regular fa-star"></i> -->
             </div>
             
-
-            <!-- <p class="vote">{{Math.ceil(item.vote_average / 2)}}</p> -->
+            <p class="overview">{{item.overview}}</p>
         </div>
+        </div>
+
+        
         
     </div>
 </template>
@@ -77,20 +77,20 @@
             width: 100%;
             transform: translate(-50%, 0);
         }
-        /* img{
+        img{
             width: 342px;
             height: 513px;
-        } */
+        }
 
         h1{
             top: 20px;
         }
 
         h2{
-            top: 200px;
+            top: 100px;
         }
         p.language, .language{
-            top: 300px;
+            top: 180px;
         }
         .language{
             width: 50px;
@@ -99,7 +99,12 @@
         }
         div.vote{
             color: yellow;
-            top: 400px;
+            top: 230px;
+        }
+        p.overview{
+            top: 280px;
+            padding: 15px;
+            text-align: left;
         }
     }
 </style>
